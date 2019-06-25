99b042a9a6045520eb93ea15059a8d2813435548
cuishuai <225cuishuai@gmail.com>
2019-06-25 10:25:42 +0800

    update rm script

--- rm_backup-python36.py
+++ rm_backup-python36.py
@@ -3,6 +3,7 @@
 
 import  os
 import time
+import shutil
 
 cur_time = time.time()
 #获取当天零点的时间戳
@@ -13,30 +14,38 @@ dir_path = "/data/backup"
 #删除
 def drop_file(file_dir):
     for filename in os.listdir(file_dir):
-        if os.path.isfile(file_dir + "/" +filename):
-            ft = os.stat(file_dir + "/" +filename)
-            lt = int(ft.st_mtime)
-            nt = cur_time - 3600*2 #获取当天保留截止的时间戳
-            dt = st - 86400*5      #获取保留天数的时间戳
-            
-            #delete  file  by hour in today
-            if st < lt < nt:
-                try:
+        # if os.path.isfile(file_dir + "/" +filename):
+        ft = os.stat(file_dir + "/" + filename)
+        lt = int(ft.st_mtime)
+        nt = cur_time - 3600 * 2  # 获取当天保留截止的时间戳
+        dt = st - 86400 * 5  # 获取保留天数的时间戳
+
+        # delete  file  by hour in today
+        if st < lt < nt:
+            try:
+                if os.path.isfile(file_dir + "/" + filename):
                     os.remove(file_dir + "/" + filename)
-                    os.removedirs(file_dir + "/" + filename)
                     time.sleep(3)
-                    print ("Deleted success" + file_dir + "/" + filename)
-                except (SyntaxError,IOError) as e:
-                    print (e)
-            #delete file by day
-            elif lt < dt:
-                try:
+                    print("Deleted success" + file_dir + "/" + filename)
+                else:
+                    shutil.rmtree(file_dir + "/" + filename)
+                    time.sleep(3)
+                    print("Deleted success" + file_dir + "/" + filename)
+            except (SyntaxError, IOError) as e:
+                print(e)
+        # delete file by day
+        elif lt < dt:
+            try:
+                if os.path.isfile(file_dir + "/" + filename):
                     os.remove(file_dir + "/" + filename)
-                    os.removedirs(file_dir + "/" + filename)
                     time.sleep(3)
-                    print ("Deleted success" + file_dir + "/" + filename)
-                except (SyntaxError,IOError) as e:
-                    print (e)
+                    print("Deleted success" + file_dir + "/" + filename)
+                else:
+                    shutil.rmtree(file_dir + "/" + filename)
+                    time.sleep(3)
+                    print("Deleted success" + file_dir + "/" + filename)
+            except (SyntaxError, IOError) as e:
+                print(e)
 if __name__ == '__main__':
    
     drop_file(dir_path)
